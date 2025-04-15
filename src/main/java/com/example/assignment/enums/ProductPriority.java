148a6da43a6e217325c1842e4c51a67c2db03a5c
Le Hai Duy <lehaiduy2003@gmail.com>
2025-04-15 16:09:35 +0700

    feat: use Json annotation for parsing

--- src/main/java/com/example/assignment/enums/ProductPriority.java
+++ src/main/java/com/example/assignment/enums/ProductPriority.java
@@ -1,6 +1,8 @@
 package com.example.assignment.enums;
 
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 
 @Getter
@@ -13,5 +15,28 @@ public enum ProductPriority {
 
     ProductPriority(String value) {
         this.value = value;
+    }
+
+    /**
+     * Convert a string value to a ProductPriority enum.
+     * @param value the string value to convert
+     * @return the corresponding ProductPriority enum
+     */
+    @JsonCreator
+    public static ProductPriority fromValue(String value) {
+        for (ProductPriority priority : ProductPriority.values()) {
+            if (priority.name().equalsIgnoreCase(value)) {
+                return priority;
+            }
+        }
+        throw new IllegalArgumentException("Invalid product priority: " + value);
+    }
+    /**
+     * Convert the ProductPriority enum to its string value.
+     * @return the string value of the ProductPriority enum
+     */
+    @JsonValue
+    public String toValue() {
+        return this.name();
     }
 }
