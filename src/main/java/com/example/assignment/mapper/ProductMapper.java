6e67afe41bdf2ca0b6a846472670c33b7a86a56e
Le Hai Duy <lehaiduy2003@gmail.com>
2025-04-16 22:21:54 +0700

    refactor: use base PagingResultMapper

---
+++ src/main/java/com/example/assignment/mapper/ProductMapper.java
@@ -0,0 +1,34 @@
+package com.example.assignment.mapper;
+
+import com.example.assignment.dto.request.ProductCreationReq;
+import com.example.assignment.dto.response.ProductDtoRes;
+import com.example.assignment.entity.Product;
+import com.example.assignment.mapper.helper.CategoryMappingHelper;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+/**
+ * Mapper interface for mapping between Product entity and Product DTOs.
+ * This interface uses MapStruct to generate the implementation at compile time.
+ */
+@Mapper(componentModel = "spring", uses = {CategoryMappingHelper.class})
+public interface ProductMapper extends PagingResultMapper {
+    /**
+     * Maps a Product entity to a Product DTO.
+     *
+     * @param product the Product entity to map
+     * @return the mapped Product DTO
+     */
+    ProductDtoRes toDto(Product product);
+    /**
+     * Maps a productCreationReq DTO to a Product entity.
+     * This method uses a custom mapping for the category field. <br/>
+     * This mapper relies on the {@link CategoryMappingHelper} for mapping category-related fields.
+     *
+     * @param productCreationReq the productCreationReq DTO to map
+     * @return the mapped Product entity
+     */
+    @Mapping(source = "categoryId", target = "category", qualifiedByName = "mapCategoryFromId")
+    Product toEntity(ProductCreationReq productCreationReq);
+
+}
