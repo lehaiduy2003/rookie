64ec1ea865b7c4479ad1c265d2d67da514549201
Le Hai Duy <lehaiduy2003@gmail.com>
2025-04-17 14:25:17 +0700

    refactor: reuse pagingService & fix mirror issue with enum value

--- src/main/java/com/example/assignment/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/example/assignment/service/impl/CustomerServiceImpl.java
@@ -1,51 +1,50 @@
 package com.example.assignment.service.impl;
 
-import com.example.assignment.dto.response.PagingResult;
-import com.example.assignment.dto.response.UserDtoRes;
+import com.example.assignment.dto.response.PagingRes;
+import com.example.assignment.dto.response.UserRes;
 import com.example.assignment.entity.Customer;
 import com.example.assignment.enums.MemberTier;
-import com.example.assignment.mapper.UserMapper;
 import com.example.assignment.repository.CustomerRepository;
 import com.example.assignment.service.CustomerService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import com.example.assignment.service.impl.paging.CustomerPagingServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 
 @Service
+@RequiredArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
     private final CustomerRepository customerRepository;
-    private final UserMapper userMapper;
-
-    public CustomerServiceImpl(CustomerRepository customerRepository, UserMapper userMapper) {
-        this.customerRepository = customerRepository;
-        this.userMapper = userMapper;
-    }
+    private final CustomerPagingServiceImpl customerPagingService;
 
     @Override
     public void updateMemberTier(Long customerId, String memberTier) {
         Customer customer = customerRepository.findById(customerId).orElseThrow(
             () -> new UsernameNotFoundException("Customer not found")
         );
-        customer.setMemberTier(MemberTier.valueOf(memberTier));
+        // Uppercase the memberTier to match the enum values
+        customer.setMemberTier(MemberTier.valueOf(memberTier.toUpperCase()));
         customerRepository.save(customer);
     }
 
     @Override
-    public PagingResult<UserDtoRes> getCustomersByTier(String memberTier, Integer pageNo, Integer pageSize, String sortBy) {
-        Pageable paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
-        Page<Customer> customerPage = customerRepository.findByMemberTier(MemberTier.valueOf(memberTier), paging);
-        return userMapper.toPagingResult(customerPage, userMapper::toDto);
+    public PagingRes<UserRes> getCustomersByTier(String memberTier, Integer pageNo, Integer pageSize, String sortDir, String sortBy) {
+        try {
+            // Must be in uppercase to match the enum values
+            return customerPagingService.getByCriteria(memberTier.toUpperCase(), pageNo, pageSize, sortDir, sortBy);
+        } catch (Exception e) {
+            throw new UsernameNotFoundException("No customers found");
+        }
     }
 
     @Override
-    public PagingResult<UserDtoRes> getCustomers(Integer pageNo, Integer pageSize, String sortBy) {
-        Pageable paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
-        Page<Customer> customerPage = customerRepository.findAll(paging);
-        return userMapper.toPagingResult(customerPage, userMapper::toDto);
+    public PagingRes<UserRes> getCustomers(Integer pageNo, Integer pageSize, String sortDir, String sortBy) {
+        try {
+            return customerPagingService.getMany(pageNo, pageSize, sortDir, sortBy);
+        } catch (Exception e) {
+            throw new UsernameNotFoundException("No customers found");
+        }
     }
 
 
