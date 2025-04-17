b097f083152e1424dbbcedef681c617424a68e32
Le Hai Duy <lehaiduy2003@gmail.com>
2025-04-17 14:19:56 +0700

    refactor: changing name effected

--- src/main/java/com/example/assignment/mapper/ProductMapper.java
+++ src/main/java/com/example/assignment/mapper/ProductMapper.java
@@ -1,7 +1,8 @@
 package com.example.assignment.mapper;
 
 import com.example.assignment.dto.request.ProductCreationReq;
-import com.example.assignment.dto.response.ProductDtoRes;
+import com.example.assignment.dto.response.ProductDetailRes;
+import com.example.assignment.dto.response.ProductRes;
 import com.example.assignment.entity.Product;
 import com.example.assignment.mapper.helper.CategoryMappingHelper;
 import org.mapstruct.Mapper;
@@ -11,15 +12,24 @@ import org.mapstruct.Mapping;
  * Mapper interface for mapping between Product entity and Product DTOs.
  * This interface uses MapStruct to generate the implementation at compile time.
  */
-@Mapper(componentModel = "spring", uses = {CategoryMappingHelper.class})
-public interface ProductMapper extends PagingResultMapper {
+@Mapper(componentModel = "spring", uses = {CategoryMappingHelper.class, UserMapper.class})
+public interface ProductMapper extends PagingMapper {
     /**
      * Maps a Product entity to a Product DTO.
      *
      * @param product the Product entity to map
      * @return the mapped Product DTO
      */
-    ProductDtoRes toDto(Product product);
+    ProductRes toDto(Product product);
+    /**
+     * Maps a Product entity to a Product detail DTO.
+     *
+     * @param product the Product entity to map
+     * @return the mapped Product detail DTO
+     */
+    @Mapping(source = "createdBy", target = "createdBy")
+    @Mapping(source = "ratings", target = "ratings")
+    ProductDetailRes toDetailsDto(Product product);
     /**
      * Maps a productCreationReq DTO to a Product entity.
      * This method uses a custom mapping for the category field. <br/>
