12d3b69efe129f694f659edfe0dd0f1badad3624
Le Hai Duy <lehaiduy2003@gmail.com>
2025-04-18 18:00:37 +0700

    fix: handle exception for user apis

--- src/main/java/com/example/assignment/controller/UserController.java
+++ src/main/java/com/example/assignment/controller/UserController.java
@@ -3,12 +3,15 @@ package com.example.assignment.controller;
 import com.example.assignment.dto.response.PagingRes;
 import com.example.assignment.dto.response.UserDetailsRes;
 import com.example.assignment.dto.response.UserRes;
+import com.example.assignment.exception.ResourceAlreadyExistException;
 import com.example.assignment.service.UserService;
 import com.example.assignment.dto.request.UserCreationReq;
 import com.example.assignment.dto.request.UserInfoUpdatingReq;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -27,6 +30,10 @@ public class UserController {
         try {
             UserRes createdUser = userService.createUser(userCreationReq);
             return ResponseEntity.status(201).body(createdUser);
+        } catch (ResourceAlreadyExistException e) {
+            return ResponseEntity.status(409).build(); // Conflict
+        } catch (AccessDeniedException e) {
+            return ResponseEntity.status(403).build(); // Forbidden
         } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
         }
@@ -38,10 +45,13 @@ public class UserController {
         try {
             UserRes updatedUser = userService.updateUserById(id, userInfoUpdatingReq);
             return ResponseEntity.ok(updatedUser);
+        } catch (UsernameNotFoundException e) {
+            return ResponseEntity.status(404).build(); // Not Found
+        } catch (AccessDeniedException e) {
+            return ResponseEntity.status(403).build(); // Forbidden
         } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
         }
-
     }
 
     @PreAuthorize("hasRole('ADMIN')")
@@ -50,6 +60,10 @@ public class UserController {
         try {
             userService.deleteUserById(id);
             return ResponseEntity.noContent().build();
+        } catch (UsernameNotFoundException e) {
+            return ResponseEntity.status(404).build(); // Not Found
+        } catch (AccessDeniedException e) {
+            return ResponseEntity.status(403).build(); // Forbidden
         } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
         }
@@ -66,6 +80,10 @@ public class UserController {
         try {
             PagingRes<UserRes> users = userService.getUsers(pageNo, pageSize, sortDir, sortBy);
             return ResponseEntity.ok(users);
+        } catch (UsernameNotFoundException e) {
+            return ResponseEntity.status(404).build(); // Not Found
+        } catch (AccessDeniedException e) {
+            return ResponseEntity.status(403).build(); // Forbidden
         } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
         }
@@ -76,6 +94,8 @@ public class UserController {
         try {
             UserDetailsRes user = userService.getUserById(id);
             return ResponseEntity.ok(user);
+        } catch (UsernameNotFoundException e) {
+            return ResponseEntity.status(404).build(); // Not Found
         } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
         }
