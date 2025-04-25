353441fb79672a2d472f1ba30558e955973386b5
Le Hai Duy <lehaiduy2003@gmail.com>
2025-04-25 16:15:29 +0700

    refactor: refactor filter to remove unnecessary if conditions

--- src/main/java/com/example/assignment/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/assignment/filter/JwtAuthenticationFilter.java
@@ -12,7 +12,6 @@ import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -21,12 +20,11 @@ import java.io.IOException;
  * Filter for JWT authentication.
  * Intercepts requests and validates JWT tokens.
  */
-@Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtProvider jwtProvider;
-    private final UserService userDetailsService;
+    private final UserService userService;
 
     @Override
     protected void doFilterInternal(
@@ -34,51 +32,44 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
+
+        logger.info("JWT Authentication Filter triggered");
+
         final String authHeader = request.getHeader("Authorization");
-        final String jwt;
-        final String userEmail;
 
         // Check if the Authorization header is present and starts with "Bearer"
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
-        // Extract JWT token from the Authorization header
-        jwt = authHeader.substring(7);
-        // Check if the JWT token is valid
-        if (jwt.isEmpty() || !jwtProvider.validateToken(jwt)) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
-        // start setting authentication
-        // Extract username from JWT token
-        userEmail = jwtProvider.extractUsername(jwt);
-
-        // Check if the username is not null and authentication is not yet set
-        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            // Load user details from database
-            User user = (User) this.userDetailsService.loadUserByUsername(userEmail);
-
-            // check if this token's owner is the same as the one in the database
-            if(!userEmail.equals(user.getEmail())) {
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+           final String jwt = authHeader.substring(7);
+            // Check if the JWT token is valid
+            if (jwt.isEmpty() || Boolean.FALSE.equals(jwtProvider.validateToken(jwt))) {
                 filterChain.doFilter(request, response);
                 return;
             }
+            // Extract username from JWT token
+           final String userEmail = jwtProvider.extractUsername(jwt);
+            // Check if the username is not null and authentication is not yet set
+            if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                // Load user details from database
+                User user = (User) this.userService.loadUserByUsername(userEmail);
 
-            // Create an authentication token
-            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                user,
-                null,
-                user.getAuthorities()
-            );
+                // check if this token's owner is the same as the one in the database
+                if(!userEmail.equals(user.getEmail())) {
+                    filterChain.doFilter(request, response);
+                    return;
+                }
 
-            // Set details for the authentication token
-            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                // Create an authentication token
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        user,
+                        null,
+                        user.getAuthorities()
+                );
+                // Set details for the authentication token
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                // Set authentication in SecurityContext
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
 
-            // Set authentication in SecurityContext
-            SecurityContextHolder.getContext().setAuthentication(authToken);
         }
         // Continue filter chain
         filterChain.doFilter(request, response);
